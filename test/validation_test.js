25fcfc5ff385e5b52d31393559d48a26327a6785
ifalldev <cbatista.93@gmail.com>
2016-12-31 13:46:55 -0200

    disallows invalid record from being saved validation test

---
+++ test/validation_test.js
@@ -0,0 +1,33 @@
+'use strict'
+const assert = require('assert');
+const User   = require('../src/user');
+
+describe('Validating records', () => {
+  it('requires a user name', () => {
+    const user = new User({ name: undefined});
+
+    const validationResult = user.validateSync();
+    const message = validationResult.errors.name.message;
+
+    assert(message === 'Name is required.');
+  });
+
+  it('Requires a user\' name longer then 2 characters', () => {
+    const user = new User({ name: 'Al' });
+
+    const validationResult = user.validateSync();
+    const message          = validationResult.errors.name.message;
+
+    assert(message === 'Name must be longer than 2 characters.');
+  });
+
+  it('Disallows invalid record from being saved', done => {
+    const user = new User({ name: 'Al' });
+    user.save()
+      .catch(validationResult => {
+        const message = validationResult.errors.name.message;
+        assert(message === 'Name must be longer than 2 characters.');
+        done();
+      });
+  });
+});
